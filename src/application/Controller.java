7db76b0bfdb85eb18e5145537f5425f15480fcb4
ICE-BREAKER2353 <amin.said.1@ens.etsmtl.ca>
2024-03-19 16:09:46 -0400

    C'est moi le roi ici
    Welcome to my kingdom

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,6 +1,12 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+
 public class Controller {
+	
+	@FXML
+	MenuBar barDeMenu;
 
 	public void initialize() {
 		
--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class Demo extends Application{
+public class Main extends Application{
 
 		@Override
 		public void start(Stage primaryStage) {
